e731bd6d8560c47eb7dbf8a48d49208ab1993486
hamza5word <hamza_hm@hotmail.be>
2021-11-26 18:33:29 +0100

    Initial commit

---
+++ src/main/java/com/hmz/cinemaprojectbackend/CinemaProjectBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.hmz.cinemaprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaProjectBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaProjectBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hmz/cinemaprojectbackend/CinemaProjectBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmz.cinemaprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaProjectBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
